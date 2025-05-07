2a2212d27aa1fe000917191e0081a099e9718983
Miharisoa Rakotonirina <hei.miharisoa@gmail.com>
2025-05-07 13:17:35 +0300

    feat : Add SeasonToCreate class

---
+++ src/main/java/org/hei/school/fifa_foot_api/model/SeasonToCreate.java
@@ -0,0 +1,11 @@
+package org.hei.school.fifa_foot_api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SeasonToCreate {
+    private Integer year;
+    private String alias;
+}
