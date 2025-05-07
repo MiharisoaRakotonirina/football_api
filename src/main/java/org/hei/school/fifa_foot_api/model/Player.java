3af5850ba618699aac5a0c00d7a67ca50bf1521d
Miharisoa Rakotonirina <hei.miharisoa@gmail.com>
2025-05-07 13:14:34 +0300

    feat : Add Player class

---
+++ src/main/java/org/hei/school/fifa_foot_api/model/Player.java
@@ -0,0 +1,41 @@
+package org.hei.school.fifa_foot_api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class Player {
+    private UUID id;
+    private String name;
+    private int number;
+    private PlayerPosition position;
+    private String nationality;
+    private int age;
+    private Club club;
+
+    public Player(UUID id, String name, int number, PlayerPosition position, String nationality, int age, Club club) {
+        this.id = id != null ? id : UUID.randomUUID();
+        this.name = name;
+        this.number = number;
+        this.position = position;
+        this.nationality = nationality;
+        this.age = age;
+        this.club = club;
+    }
+
+    @Override
+    public String toString() {
+        return "Player { " +
+                " id = " + id +
+                ", name = '" + name + '\'' +
+                ", number = " + number +
+                ", position = " + position +
+                ", nationality = '" + nationality + '\'' +
+                ", age = " + age +
+                ", club = " + club +
+                '}';
+    }
+}
