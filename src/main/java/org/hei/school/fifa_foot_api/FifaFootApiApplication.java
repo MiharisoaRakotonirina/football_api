7758d3062d56c318412c6555a2b06596dd84c47e
Miharisoa Rakotonirina <hei.miharisoa@gmail.com>
2025-05-07 13:33:04 +0300

    feat: Add SpringBoot main class

---
+++ src/main/java/org/hei/school/fifa_foot_api/FifaFootApiApplication.java
@@ -0,0 +1,13 @@
+package org.hei.school.fifa_foot_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifaFootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FifaFootApiApplication.class, args);
+	}
+
+}
