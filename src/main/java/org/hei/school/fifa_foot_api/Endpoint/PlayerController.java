dcb718821a11e21f9a8495b2f70b5c428da1455b
Miharisoa Rakotonirina <hei.miharisoa@gmail.com>
2025-05-07 15:02:07 +0300

    feat : Add new endpoint to get player's stats

--- src/main/java/org/hei/school/fifa_foot_api/Endpoint/PlayerController.java
+++ src/main/java/org/hei/school/fifa_foot_api/Endpoint/PlayerController.java
@@ -1,11 +1,15 @@
 package org.hei.school.fifa_foot_api.Endpoint;
 
 import org.hei.school.fifa_foot_api.model.Player;
+import org.hei.school.fifa_foot_api.model.PlayerStatistics;
 import org.hei.school.fifa_foot_api.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 
 @RequestMapping("/players")
@@ -31,5 +35,17 @@ public class PlayerController {
     @PutMapping
     public List<Player> createOrUpdatePlayers(@RequestBody(required = true) List<Player> players) {
         return playerService.saveOrUpdatePlayers(players);
+    }
+
+    @GetMapping("/{id}/statistics/{seasonYear}")
+    public ResponseEntity<PlayerStatistics> getStatisticsOfPlayerById(
+            @PathVariable("id") UUID id,
+            @PathVariable("seasonYear") int seasonYear) {
+
+        PlayerStatistics stats = playerService.getStatisticsOfPlayer(id, seasonYear);
+        if (stats == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(stats);
     }
 }
