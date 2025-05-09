df61d60b8c5258861592d6fa296dad8d392024cf
Miharisoa Rakotonirina <hei.miharisoa@gmail.com>
2025-05-09 22:19:10 +0300

    feat (Club) : Add ClubDAO interface

---
+++ src/main/java/org/hei/school/fifa_foot_api/dao/Club/ClubDAO.java
@@ -0,0 +1,16 @@
+package org.hei.school.fifa_foot_api.dao.Club;
+
+import org.hei.school.fifa_foot_api.model.Club;
+import org.hei.school.fifa_foot_api.model.Player;
+import org.hei.school.fifa_foot_api.model.SimplePlayer;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ClubDAO {
+    List<Club> getAllClubs();
+    List<Club> saveOrUpdateClubs(List<Club> clubs);
+    List<SimplePlayer> findPlayersOfClub(UUID clubId);
+    Club findById(UUID id);
+    List<Player> replacePlayersOfClub(UUID clubId, List<Player> players);
+}
