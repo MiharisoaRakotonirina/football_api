ebcf07766ff5534d54262ba5e62352aff6099f96
Miharisoa Rakotonirina <hei.miharisoa@gmail.com>
2025-05-09 20:32:10 +0300

    feat(club) : Add new endpoint to get all players for a specific club

--- src/main/java/org/hei/school/fifa_foot_api/Endpoint/ClubController.java
+++ src/main/java/org/hei/school/fifa_foot_api/Endpoint/ClubController.java
@@ -1,12 +1,16 @@
 package org.hei.school.fifa_foot_api.Endpoint;
 
 import org.hei.school.fifa_foot_api.model.Club;
+import org.hei.school.fifa_foot_api.model.SimplePlayer;
 import org.hei.school.fifa_foot_api.service.ClubService;
 import org.hei.school.fifa_foot_api.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/clubs")
@@ -26,5 +30,14 @@ public class ClubController {
     @PutMapping
     public List<Club> createOrUpdateClubs(@RequestBody(required = true) List<Club> clubs) {
         return clubService.saveOrUpdateClubs(clubs);
+    }
+
+    @GetMapping("/{id}/players")
+    public ResponseEntity<?> getPlayersOfClub(@PathVariable(required = true) UUID id) {
+        if (clubService.findById(id) == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Club not found");
+        }
+        List<SimplePlayer> players = clubService.findPlayersOfClub(id);
+        return ResponseEntity.ok(players);
     }
 }
